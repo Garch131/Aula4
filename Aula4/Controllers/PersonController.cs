d31130a7bd24937fcc3ea884dd6c557b9bbbfd17
240541 <240541@LTI5104>
2026-04-07 16:26:47 -0300

    Adicionar arquivos de projeto.

---
+++ Aula4/Controllers/PersonController.cs
@@ -0,0 +1,130 @@
+﻿using Aula4.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Aula4.Controllers
+{
+    public class PersonController : Controller
+    {
+
+        // GET: PersonController
+        public ActionResult Index()
+        {
+            
+            var lista = Ler();
+            
+            
+
+            //var lista = Person.Lista;
+
+            return View(lista);
+        }
+
+        private List<Person> Iniciar()
+        {
+            var lista = Person.Lista;
+            if (Ler().Count == 0) Gravar(lista);
+            return lista;
+            
+        }
+        private List<Person> Ler()
+
+        {
+            var lista = Person.Lista;
+            string people = HttpContext.Session.GetString("people");
+            
+            if (String.IsNullOrEmpty(people))
+            {
+                lista = Iniciar();
+            }
+            else
+            {
+                lista = JsonConvert.DeserializeObject<List<Person>>(people);
+            }
+                return lista;
+        }
+
+        private void Gravar(List<Person> lista)
+        {
+            string people;
+            people = JsonConvert.SerializeObject(lista);
+            HttpContext.Session.SetString("people", people);
+        }
+
+        private void GerarListaNaSessao()
+        {
+            string people = JsonConvert.SerializeObject(Person.Lista);
+            HttpContext.Session.SetString("people", people);
+        }
+
+        // GET: PersonController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(Person.Lista[id]);
+        }
+
+        // GET: PersonController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PersonController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PersonController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PersonController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Aula4/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace Aula4.Models
+{
+    public class Person
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public static List<Person> Lista
+        {
+            get
+            {
+                var lista = new List<Person>();
+                lista.Add(new Person { Nome = "Lisa", Idade = 17 });
+                lista.Add(new Person { Nome = "LiKaDuo", Idade = 17 });
+                lista.Add(new Person { Nome = "Juan", Idade = 16 });
+
+                return lista;
+            }
+        }
+    }
+}
